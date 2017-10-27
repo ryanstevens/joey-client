137591046d14aa21d5867ac5df35372e66a4fa9b
Ryan Stevens <stevens.ryan@gmail.com>
2017-10-26 20:25:11 -0700

    adding reducers

--- src/Root.js
+++ src/Root.js
@@ -1,7 +1,35 @@
 import React, { Component } from 'react';
-import { Image, StyleSheet, Text, View, StatusBarIOS, Platform } from 'react-native';
+import { Text, View } from 'react-native';
 import Plaid from '@components/plaid';
 import Welcome from '@components/welcome';
+import { Provider } from 'react-redux'
+import { combineReducers, createStore } from 'redux';
+import appReducers from 'reducers';
+console.log(appReducers)
+const rootReducer = (state, action) => {
+  return appReducers(state, action);
+};
+
+const store = createStore(rootReducer);
+
+class Foo extends Component {
+ 
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return <View>
+      <Text>hi</Text>
+      <Text>hi</Text>
+      <Text>hi</Text>
+      <Text>hi</Text>
+      <Text>hi</Text>
+      <Text>hi</Text>
+      <Text>hi</Text>
+    </View>;
+  }
+}
 
 export default class App extends Component {
 
@@ -12,20 +40,23 @@ export default class App extends Component {
     };
   }
   render() {
-    if (this.state.view === 'login') {
-      return (
-        <View style={styles.container}>
-          <Welcome.component onPress = {this.press} />
-        </View>
-      );
-    }
-    else {
-      return (
-        <Plaid.component  
-           onMessage = {this.onMessage}
-        />
-      )
-    }
+    return (
+      <Provider store={store}>
+        <Foo />
+      </Provider>
+    ) 
+      // if (this.state.view === 'login') {
+      //   return (
+      //     <Welcome.component onPress = {this.press} />
+      //   );
+      // }
+      // else {
+      //   return (
+      //     <Plaid.component  
+      //       onMessage = {this.onMessage}
+      //     />
+      //   )
+      // }
   }
 
   press = () => {
@@ -37,13 +68,3 @@ export default class App extends Component {
     console.log(e);
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    paddingTop: 30,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1'
-  }
-});
