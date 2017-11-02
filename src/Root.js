9ed5a40c7e08acf276476b65dea8686466300cdd
Ryan Stevens <stevens.ryan@gmail.com>
2017-11-02 00:10:02 -0700

    added login scaffolding

--- src/Root.js
+++ src/Root.js
@@ -6,41 +6,6 @@ import { Provider, connect } from 'react-redux'
 import { combineReducers, createStore } from 'redux';
 
 const Navigation = components.nav;
-
-// const AppNavigator = StackNavigator({
-//   welcome: {
-//     screen: components.welcome
-//   },
-//   plaid: {
-//     screen: components.plaid
-//   }
-// });
-
-// const initialState = AppNavigator.router.getStateForAction(AppNavigator.router.getActionForPathAndParams('welcome'));
-
-// const navReducer = (state = initialState, action) => {
-//   const nextState = AppNavigator.router.getStateForAction(action, state);
-//   return nextState || state;
-// };
-
-// const store = createStore(combineReducers(Object.assign(
-//   {}, 
-//   reducers,
-//   { nav: navReducer }
-// )));
-
-// class App extends React.Component {
-//   render() {
-//     return (
-//       <AppNavigator navigation={addNavigationHelpers({
-//         dispatch: this.props.dispatch,
-//         state: this.props.nav,
-//       })} />
-//     );
-//   }
-// }
-
-
 const store = createStore(reducers);
 
 // we only really need export a function here
